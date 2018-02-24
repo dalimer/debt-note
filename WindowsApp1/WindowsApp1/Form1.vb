7485dc51fc545f8315d826e3ebb0483d46f4f2ef
dalimer <36789641+dalimer@users.noreply.github.com>
2018-02-24 22:26:37 +0800

    Added the directory for the project
    it can save values to database debtprofile

---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,28 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim MysqlConn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MysqlConn = New MySqlConnection
+        MysqlConn.ConnectionString = "server=localhost;userid=root;password=;database=debtprofile"
+        Dim READER As MySqlDataReader
+
+        Try
+            MysqlConn.Open()
+            Dim Query As String
+            Query = "insert into loaners(prof_id, prof_fname, prof_lname, prof_age) values ('asd','asdd','asdd','asdas')"
+            COMMAND = New MySqlCommand(Query, MysqlConn)
+            READER = COMMAND.ExecuteReader
+
+            MessageBox.Show("Data saved")
+            MysqlConn.Clone()
+
+
+        Catch ex As MySqlException
+            MessageBox.Show(ex.Message)
+        Finally
+            MysqlConn.Dispose()
+        End Try
+    End Sub
+End Class
